4b477fe1a1f5a114e91bed3f1cc7d048a33028bf
JAAR91 <jaarriagaramos@gmail.com>
2021-06-10 13:35:33 -0600

    add another test to the followers table

--- spec/models/follower_spec.rb
+++ spec/models/follower_spec.rb
@@ -2,11 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Follower, type: :model do
   describe 'Follower' do
-    it 'users have inverted followers' do
+    it 'users have many followers' do
       user1 = User.create(name: 'first user', username: 'followtest1', password: 'password')
       user2 = User.create(name: 'second user', username: 'followtest2', password: 'password')
       user1.followers.create(follow_id: user2.id)
       expect(user2.inverted_followers.count).to eq(1)
+    end
+
+    it 'users can follow manby people' do
+      user1 = User.create(name: 'first user', username: 'followtest1', password: 'password')
+      user2 = User.create(name: 'second user', username: 'followtest2', password: 'password')
+      user3 = User.create(name: 'second user', username: 'followtest3', password: 'password')
+      user1.followers.create(follow_id: user2.id)
+      user1.followers.create(follow_id: user3.id)
+      expect(user1.followers.count).to eq(2)
     end
   end
 end
