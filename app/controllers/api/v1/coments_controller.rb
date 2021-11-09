f424663f2fc742ad4d3d7f40d4eabc4721f3d2c2
jaar91 <jaarriagaramos@gmail.com>
2021-11-09 12:01:44 -0600

    test endpoints using the token

--- app/controllers/api/v1/coments_controller.rb
+++ app/controllers/api/v1/coments_controller.rb
@@ -1,11 +1,9 @@
 class Api::V1::ComentsController < ApplicationController
 
-    skip_before_action :authorize_request
     skip_before_action :verify_authenticity_token
   
     def create
         @user = User.find_by(username: body_params[:username])
-        # render json: body_params[:post_id]
         @coment = Coment.new(post_id: body_params[:post_id], user_id: @user.id, body: body_params[:comment])
         if @coment.save
             render json: "Your comment was created."
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::PostsController < ApplicationController
-  skip_before_action :authorize_request
-  skip_before_action :verify_authenticity_token
 
+  skip_before_action :verify_authenticity_token
+  
   def index 
     @posts = Post.all
     render json: @posts
